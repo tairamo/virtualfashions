a0627a69a2e3d7eb501bcee85051227d3e6e8aa1
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-04-29 15:30:05 +0100

    Update JoinCommunity text

--- frontend/src/components/JoinCommunity/index.js
+++ frontend/src/components/JoinCommunity/index.js
@@ -12,19 +12,19 @@ const JoinCommunity = () => {
                 <div>
                     <div>
                         <Body>
-                            <h2>Junte-se à comunidade Upvote para acessar as ferramentas do criador.</h2>
+                            <h2>Join the Community Upvote to access creator tools.</h2>
                             <BodyLast>
                                 <BodyText>
-                                Com o Community Upvote, os artistas são incentivados a apoiar outros artistas e a preparar o terreno para um modelo de curadoria liderada pela comunidade que coloca o poder nas mãos dos criadores.
+                                With the Community Upvote, artists are encouraged to support other artists and to set the stage for a model of community-led curation that puts power in the hands of creators.
                                 </BodyText>
                                 <BodyText>
-                                Na nova economia criativa, todos nós crescemos juntos.
+                                In the new creative economy, we all rise up together.
                                 </BodyText>
-                                <BodyButton><button>Junte-se à comunidade Upvote</button></BodyButton>
+                                <BodyButton><button>Join Community Upvote</button></BodyButton>
                             </BodyLast>
                         </Body>
                         <Footer>
-                            <BodyText>Número atual de perfis no Upvote da comunidade:</BodyText>
+                            <BodyText>Current number of profiles on the Community Upvote:</BodyText>
                             <div>12</div>
                         </Footer>
                     </div>
